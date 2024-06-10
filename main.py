e55c0a24816389a47baf78ab6b70bdba38122f67
claudio <cgimenez@fi.uba.ar>
2024-06-10 00:05:35 +0000

    solucionado agregar y ver heroe

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.exc import SQLAlchemyError
 from models import db, Heroes
 
-db_url = "postgresql+psycopg2://postgres:postgres@localhost:5432/postgres"
+db_url = "postgresql+psycopg2://postgres:postgres@localhost:5432/heroes"
 try:
     # Intenta crear una conexión al motor de la base de datos
     engine = create_engine(db_url)
@@ -52,6 +52,40 @@ def get_heroes():
     except Exception as error:
         print('Error', error)
         return jsonify({'message': 'Internal server errores'}), 500
+
+@app.route("/heroes",methods=['POST'])
+def add_heroes():
+    try:
+        data= request.json
+        name= data.get('name')
+        if not name :
+            return jsonify({'message': 'Bad request, name not found'}), 400
+        alignment= data.get('alignment')
+        if not alignment :
+            return jsonify({'message': 'Bad request, alignment not found'}), 400
+        publisher= data.get('publisher')
+        if not publisher :
+            return jsonify({'message': 'Bad request, publisher not found'}), 400
+        names= data.get('names')
+        if not names :
+            return jsonify({'message': 'Bad request, names not found'}), 400
+        race= data.get('race')
+        if not race :
+            return jsonify({'message': 'Bad request, race not found'}), 400
+        gender= data.get('gender')
+        if not gender :
+            return jsonify({'message': 'Bad request, gender not found'}), 400
+        image= data.get('image')
+        if not image :
+            return jsonify({'message': 'Bad request, image not found'}), 400
+        new_heroe = Heroes(name=name, alignment=alignment, publisher=publisher, names=names,race=race, gender=gender, image=image)
+        db.session.add(new_heroe)
+        db.session.commit()
+        return jsonify({'heroe':{'id':new_heroe.id,'name':new_heroe.name,'alignment':new_heroe.alignment, 'publisher':new_heroe.publisher,'names':new_heroe.names,'race':new_heroe.race,'gender':new_heroe.gender,'image':new_heroe.image}}),201
+    except Exception as error:
+        print('Error', error)
+        return jsonify({'message': 'Internal server error'}), 500
+
 
 if __name__ == '__main__':
     db.init_app(app)
